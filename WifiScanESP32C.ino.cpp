f0df2759414e75f3f03fa13183de221a3e80ab41
Edgar <emartinez5@inscamidemar.cat>
2025-10-20 15:46:39 +0200

    He cambiat el codi modificant l'idioma dels missatges que surten

--- WifiScanESP32C.ino.cpp
+++ WifiScanESP32C.ino.cpp
@@ -4,23 +4,23 @@
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Initializing WiFi...");
+  Serial.println("Iniciant WiFi...");
   WiFi.mode(WIFI_STA);
-  Serial.println("Setup done!");
+  Serial.println("Preparació feta!");
 }
 
 void loop() {
-  Serial.println("Scanning...");
+  Serial.println("Escanejant...");
 
   // WiFi.scanNetworks will return the number of networks found
   int n = WiFi.scanNetworks();
-  Serial.println("Scan done!");
+  Serial.println("Escanejat fet!");
   if (n == 0) {
-    Serial.println("No networks found.");
+    Serial.println("No s'han trobat xarxes.");
   } else {
     Serial.println();
     Serial.print(n);
-    Serial.println(" networks found");
+    Serial.println(" Xarxes trobades");
     for (int i = 0; i < n; ++i) {
       // Print SSID and RSSI for each network found
       Serial.print(i + 1);
